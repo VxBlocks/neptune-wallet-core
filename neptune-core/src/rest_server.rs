efc329358a5f56f9ac6a2ababad6aad4be7af593
kaola526 <huweixiong@storswift.com>
2025-12-31 09:56:45 +0800

    add mempool

--- neptune-core/src/rest_server.rs
+++ neptune-core/src/rest_server.rs
@@ -676,9 +676,12 @@ async fn broadcast_transaction(
 
     info!("broadcasted insert tx: {}", tx.kernel.txid().to_string());
 
-    // state
-    //     .mempool_insert(tx.clone(), UpgradePriority::Critical)
-    //     .await;
+    if tx.kernel.inputs.len() < 3 && tx.kernel.outputs.len() < 3 {
+        state
+        .mempool_insert(tx.clone(), UpgradePriority::Critical)
+        .await;
+    }
+    
     let _ = rpcstate
         .rpc_server_to_main_tx
         .send(RPCServerToMain::BroadcastTx(Arc::new(tx)))
